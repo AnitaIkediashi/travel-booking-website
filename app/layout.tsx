87d49fd87ac1a34b5a6a23b8fb3373097b224210
Anita Ikediashi <anitaikediashi83@gmail.com>
2025-10-29 12:13:36 +0100

    created the footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { Footer } from "@/components/navigation/footer";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -21,9 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${montserrat.variable}  antialiased`}
+        className={`${montserrat.variable}  antialiased min-h-screen`}
       >
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/navigation/footer.tsx
@@ -0,0 +1,10 @@
+
+
+export const Footer = () => {
+  return (
+    <footer className="w-full relative">
+      <div className="w-[77%] lg:h-[305px] h-auto mx-auto rounded-[20px] bg-mint-green-50 "></div>
+      <div className="absolute top-[151px] w-full lg:h-[422px] h-auto left-0 right-0 bg-mint-green-100 -z-10"></div>
+    </footer>
+  );
+}
