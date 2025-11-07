0c940d2cdbd51818a193c47d46ec2ddafbe5a0ce
Anita Ikediashi <anitaikediashi83@gmail.com>
2025-11-07 11:11:11 +0100

    made it responsive and add hover effect

--- components/navigation/home_navbar.tsx
+++ components/navigation/home_navbar.tsx
@@ -1,14 +1,22 @@
+"use client";
+
 import Image from "next/image";
 import { BedIcon } from "../icons/bed";
 import { FlightIcon } from "../icons/flight";
-import logo from '@/public/logos/light/Logo_light.svg'
+import logo from "@/public/logos/light/Logo_light.svg";
 import { Button } from "../reusable/button";
 import { MobileMenu } from "./mobile_menu";
-
+import { useState } from "react";
+import { MenuIcon } from "../icons/menu";
 
 export const HomeNavbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <section className="md:mx-[30px] md:mt-[30px] mx-3 mt-4 font-montserrat">
+    <section className="md:mx-[30px] md:mt-[30px] mx-3 mt-4 font-montserrat ">
       <div className="bg-[linear-gradient(180deg,rgba(0,0,0,0),rgba(0,0,0,0.7)),url(/landing_page/home_bg.png)] bg-no-repeat bg-center bg-cover h-[581px] rounded-3xl relative">
         <header className="px-8 py-[30px]">
           <nav className="flex items-center justify-between text-white">
@@ -32,12 +40,21 @@ export const HomeNavbar = () => {
                 label="login"
                 className="capitalize text-white text-sm font-semibold"
               />
-              <Button label="Sign up" className="text-sm font-semibold px-6 py-[15.5px] rounded-lg bg-white text-blackish-green" />
+              <Button
+                label="Sign up"
+                className="text-sm font-semibold px-6 py-[15.5px] rounded-lg bg-white text-blackish-green"
+              />
             </div>
-            <MobileMenu />
+            <div
+              className="cursor-pointer lg:hidden block"
+              onClick={handleClick}
+            >
+              <MenuIcon fillColor="#fff" />
+            </div>
           </nav>
         </header>
+        <MobileMenu showMenu={showMenu} />
       </div>
     </section>
   );
-}
+};
--- components/navigation/mobile_menu.tsx
+++ components/navigation/mobile_menu.tsx
@@ -1,15 +1,45 @@
-'use client'
-import { useState } from "react";
-import { MenuIcon } from "../icons/menu"
 
-export const MobileMenu = () => {
-    const [showMenu, setShowMenu] = useState(false)
+import { FlightIcon } from "../icons/flight";
+import { BedIcon } from "../icons/bed";
+import { Button } from "../reusable/button";
+
+type MenuProps = {
+  showMenu: boolean;
+};
+
+export const MobileMenu = ({showMenu}: MenuProps) => {
   return (
-    <div className="relative block lg:hidden">
-      <div className="cursor-pointer">
-        <MenuIcon fillColor="#fff" />
-      </div>
-      <div className="absolute bg-white h-[400px] w-[332px] rounded-lg right-0"></div>
-    </div>
+    <ul
+      className={`absolute top-[69px] lg:hidden bg-white md:w-[210px] w-44 rounded-lg flex flex-col gap-6 p-4 transition-all duration-300 ease-in-out ${
+        showMenu
+          ? "right-0 -translate-x-8  opacity-100"
+          : "right-0 translate-x-0 opacity-0"
+      }`}
+    >
+      <li className="flex items-center gap-[3.5px] cursor-pointer transition-all duration-300 ease-in-out hover:bg-blackish-green/30 hover:py-3 hover:px-2 hover:rounded-lg">
+        <FlightIcon fillColor="#112211" />
+        <span className="capitalize text-sm font-semibold text-blackish-green">
+          find flight
+        </span>
+      </li>
+      <li className="flex items-center gap-[3.5px] cursor-pointer transition-all duration-300 ease-in-out hover:bg-blackish-green/30 hover:py-3 hover:px-2 hover:rounded-lg">
+        <BedIcon fillColor="#112211" />
+        <span className="capitalize text-sm font-semibold text-blackish-green">
+          find stays
+        </span>
+      </li>
+      <li className="hover:bg-blackish-green/30 hover:py-3 hover:px-2 hover:rounded-lg transition-all duration-300 ease-in-out">
+        <Button
+          label="login"
+          className="capitalize text-blackish-green text-sm font-semibold"
+        />
+      </li>
+      <li>
+        <Button
+          label="Sign up"
+          className="text-white bg-blackish-green w-full text-sm font-semibold py-3 rounded-lg hover:bg-blackish-green/70 transition duration-300 ease-in-out"
+        />
+      </li>
+    </ul>
   );
-}
+};
