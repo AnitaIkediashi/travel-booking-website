bac7df59ff8054120a7ab5105b940896708d89f1
Anita Ikediashi <anitaikediashi83@gmail.com>
2025-11-09 19:15:48 +0100

    installed dayjs and added some functionality

--- components/reusable/flight_dropdown.tsx
+++ components/reusable/flight_dropdown.tsx
@@ -1,10 +1,36 @@
 import { Button } from "./button";
 import { CustomRadioTag } from "./custom_radio_tag";
 
-export const FlightDropdown = () => {
+type FlightDropdownProps = {
+    adultCount: number;
+    childrenCount: number;
+    cabinType: string;
+    onAdultIncrement: () => void;
+    onAdultDecrement: () => void;
+    onChildrenIncrement: () => void;
+    onChildrenDecrement: () => void;
+    onCabinClassChange: (value: string) => void;
+    showDropDown?: boolean;
+    onClose?: () => void;
+}
+
+export const FlightDropdown = ({
+  adultCount,
+  childrenCount,
+  cabinType,
+  onAdultIncrement,
+  onAdultDecrement,
+  onChildrenIncrement,
+  onChildrenDecrement,
+  onCabinClassChange,
+  showDropDown,
+  onClose
+}: FlightDropdownProps) => {
   return (
     <div
-      className={`absolute top-[42px] left-0 right-0 z-10 p-4 min-h-[200px] bg-white shadow-light rounded-sm font-montserrat text-blackish-green transition-all duration-300`}
+      className={`absolute top-[59px] left-0 right-0 z-20 p-4 min-h-[200px] bg-white shadow-light rounded-sm font-montserrat text-blackish-green ${
+        showDropDown ? "block" : "hidden"
+      }`}
     >
       <h4 className="border-b-[0.5px] border-b-blackish-green/40 mb-3 pb-2  text-sm font-semibold capitalize">
         passenger
@@ -21,12 +47,14 @@ export const FlightDropdown = () => {
             type="button"
             label="+"
             className="md:w-[30px] md:h-[30px] w-7 h-7 rounded-lg grid place-items-center border border-blackish-green/40 font-medium text-lg"
+            onClick={onAdultIncrement}
           />
-          <span>2</span>
+          <span>{adultCount}</span>
           <Button
             type="button"
             label="-"
             className="md:w-[30px] md:h-[30px] w-7 h-7 rounded-lg grid place-items-center border border-blackish-green/40 font-medium text-lg"
+            onClick={onAdultDecrement}
           />
         </div>
       </div>
@@ -42,12 +70,14 @@ export const FlightDropdown = () => {
             type="button"
             label="+"
             className="md:w-[30px] md:h-[30px] w-7 h-7 rounded-lg grid place-items-center border border-blackish-green/40 font-medium text-lg"
+            onClick={onChildrenIncrement}
           />
-          <span>2</span>
+          <span>{childrenCount}</span>
           <Button
             type="button"
             label="-"
             className="md:w-[30px] md:h-[30px] w-7 h-7 rounded-lg grid place-items-center border border-blackish-green/40 font-medium text-lg"
+            onClick={onChildrenDecrement}
           />
         </div>
       </div>
@@ -60,34 +90,40 @@ export const FlightDropdown = () => {
           value="Economy"
           outerCircleClassName="border-mint-green-100"
           innerCircleClassName="bg-mint-green-100"
-          selected="Economy"
-          onClick={(value) => value}
+          selected={cabinType}
+          onClick={(value) => onCabinClassChange(value)}
         />
         <CustomRadioTag
           label="Premium Economy"
-          value="PremiumEconomy"
+          value="Premium"
           outerCircleClassName="border-mint-green-100"
           innerCircleClassName="bg-mint-green-100"
-          selected="PremiumEconomy"
-          onClick={(value) => value}
+          selected={cabinType}
+          onClick={(value) => onCabinClassChange(value)}
         />
         <CustomRadioTag
           label="Business"
           value="Business"
           outerCircleClassName="border-mint-green-100"
           innerCircleClassName="bg-mint-green-100"
-          selected="Business"
-          onClick={(value) => value}
+          selected={cabinType}
+          onClick={(value) => onCabinClassChange(value)}
         />
         <CustomRadioTag
           label="First Class"
           value="First"
           outerCircleClassName="border-mint-green-100"
           innerCircleClassName="bg-mint-green-100"
-          selected="First"
-          onClick={(value) => value}
+          selected={cabinType}
+          onClick={(value) => onCabinClassChange(value)}
         />
       </div>
+      <Button
+        type="button"
+        label="confirm"
+        className="w-full h-12 flex items-center justify-center capitalize text-blackish-green bg-mint-green-100 hover:bg-blackish-green-10 hover:text-white transition ease-in-out duration-300 font-medium text-sm rounded-sm mt-3"
+        onClick={onClose}
+      />
     </div>
   );
 }
--- components/reusable/search_flights.tsx
+++ components/reusable/search_flights.tsx
@@ -1,67 +1,177 @@
-'use client'
+"use client";
 import { useState } from "react";
 import { SwapIcon } from "../icons/swap";
 import { DatePicker, Select } from "antd";
 import { FlightDropdown } from "./flight_dropdown";
+import dayjs from "dayjs";
 
 const { RangePicker } = DatePicker;
 
+const dateFormat = "DD MMM YY ";
+
+type InitialState = {
+  fromValue: string;
+  toValue: string;
+  trip: string;
+  entryDate: dayjs.Dayjs | null;
+  startDate: dayjs.Dayjs | null;
+  endDate: dayjs.Dayjs | null;
+  adultCount: number;
+  childrenCount: number;
+  cabinClass: string;
+};
+
 export const SearchFlights = () => {
-    const [swapInput, setSwapInput] = useState(false);
-    const [initialValues, setInitialValues] = useState({
-        fromValue: '',
-        toValue: ''
+  const [swapInput, setSwapInput] = useState(false);
+  const [initialValues, setInitialValues] = useState<InitialState>({
+    fromValue: "",
+    toValue: "",
+    trip: "",
+    entryDate: null,
+    startDate: null,
+    endDate: null,
+    adultCount: 0,
+    childrenCount: 0,
+    cabinClass: "",
+  });
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  const handleDropDownClick = () => {
+    setShowDropDown(!showDropDown);
+  };
+
+  const handleSwap = () => {
+    setSwapInput((prev) => !prev);
+  };
+
+  const handleLocationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInitialValues((prevValues) => {
+      return {
+        ...prevValues,
+        [name]: value,
+      };
     });
-    const [showDropDown, setShowDropDown] = useState(false);
+  };
 
-    const handleClick = () => {
-        setShowDropDown(prev => !prev);
-    }
+  const handleTripChange = (value: string) => {
+    setInitialValues((prevValues) => {
+      const updatedValues = {
+        ...prevValues,
+        trip: value,
+      };
 
-    const handleSwap = () => {
-        setSwapInput(prev => !prev)
-    }
+      if (value === "one-way") {
+        updatedValues.startDate = null;
+        updatedValues.endDate = null;
+      } else if (value === "round-trip") {
+        updatedValues.entryDate = null;
+      }
+      return updatedValues;
+    });
+  };
 
-    const handleLocationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        setInitialValues(prevValues => {
-            return {
-              ...prevValues,
-              [name]: value,
-            };
-        })
-    };
+  const handleSingleDateChange = (date: dayjs.Dayjs | null) => {
+    setInitialValues((prevValues) => {
+      return {
+        ...prevValues,
+        entryDate: date,
+      };
+    });
+  };
 
-    const inputClassName =
-      "outline-none text-black-20 text-base w-full caret-black-20 focus:border-2 focus:border-blue-500/10 focus:rounded-sm focus:border-b-blue-500 transition-all duration-100 ease-in-out";
+  const handleDateRangeChange = (
+    dates: [dayjs.Dayjs | null, dayjs.Dayjs | null] | null
+  ) => {
+    setInitialValues((prevValues) => {
+      return {
+        ...prevValues,
+        startDate: dates ? dates[0] : null,
+        endDate: dates ? dates[1] : null,
+      };
+    });
+  };
 
-    const inputA = (
-      <input
-        key={swapInput ? "to" : "from"}
-        name={swapInput ? "toValue" : "fromValue"}
-        type="text"
-        size={12}
-        value={swapInput ? initialValues.toValue : initialValues.fromValue}
-        className={inputClassName}
-        onChange={handleLocationChange}
-      />
-    );
-    const inputB = (
-      <input
-        key={swapInput ? "from" : "to"}
-        name={swapInput ? "fromValue" : "toValue"}
-        type="text"
-        size={12}
-        value={swapInput ? initialValues.fromValue : initialValues.toValue}
-        className={inputClassName}
-        onChange={handleLocationChange}
-      />
-    );
+  const handleAdultIncrement = () => {
+    setInitialValues((prevValues) => {
+      return {
+        ...prevValues,
+        adultCount: prevValues.adultCount + 1,
+      };
+    });
+  };
 
-    const tripOptions = [
-      { value: "one-way", label: "One way" },
-      { value: "round-trip", label: "Round trip" },
-    ];
+  const handleAdultDecrement = () => {
+    setInitialValues((prevValues) => {
+      return {
+        ...prevValues,
+        adultCount: Math.max(prevValues.adultCount - 1, 0),
+      };
+    });
+  };
+
+  const handleChildrenIncrement = () => {
+    setInitialValues((prevValues) => {
+      return {
+        ...prevValues,
+        childrenCount: prevValues.childrenCount + 1,
+      };
+    });
+  };
+
+  const handleChildrenDecrement = () => {
+    setInitialValues((prevValues) => {
+      return {
+        ...prevValues,
+        childrenCount: Math.max(prevValues.childrenCount - 1, 0),
+      };
+    });
+  };
+
+  const handleCabinClassChange = (value: string) => {
+    console.log("cabin type selected:", value);
+    setInitialValues((prevValues) => {
+      return {
+        ...prevValues,
+        cabinClass: value,
+      };
+    });
+  };
+
+  const inputClassName =
+    "outline-none text-blackish-green-10 text-base w-full focus:border-2 focus:border-blue-500/10 focus:rounded-sm focus:border-b-blue-500 transition-all duration-100 ease-in-out";
+
+  const inputA = (
+    <input
+      key={swapInput ? "to" : "from"}
+      name={swapInput ? "toValue" : "fromValue"}
+      type="text"
+      size={12}
+      value={swapInput ? initialValues.toValue : initialValues.fromValue}
+      className={inputClassName}
+      onChange={handleLocationChange}
+    />
+  );
+  const inputB = (
+    <input
+      key={swapInput ? "from" : "to"}
+      name={swapInput ? "fromValue" : "toValue"}
+      type="text"
+      size={12}
+      value={swapInput ? initialValues.fromValue : initialValues.toValue}
+      className={inputClassName}
+      onChange={handleLocationChange}
+    />
+  );
+
+  const tripOptions = [
+    { value: "one-way", label: "One way" },
+    { value: "round-trip", label: "Round trip" },
+  ];
+
+  const totalPassengers =
+    initialValues.adultCount + initialValues.childrenCount;
+
   return (
     <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[1fr_120px_1fr_1fr] xl:grid-cols-[1fr_140px_1fr_1fr] gap-6 font-montserrat">
       <div>
@@ -85,23 +195,63 @@ export const SearchFlights = () => {
       </div>
       <fieldset className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3 select_wrapper">
         <legend className="text-blackish-green text-sm capitalize">trip</legend>
-        <Select options={tripOptions} allowClear className="w-full" />
+        <Select
+          options={tripOptions}
+          allowClear
+          className="w-full text-blackish-green-10"
+          onChange={handleTripChange}
+        />
       </fieldset>
       <fieldset className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3 date_wrapper">
         <legend className="text-blackish-green text-sm capitalize">
-          depart - return
+          {initialValues.trip === "one-way"
+            ? "departure"
+            : "departure - return"}
         </legend>
-        <RangePicker />
+        {initialValues.trip === "one-way" ? (
+          <DatePicker
+            format={dateFormat}
+            className="w-full text-blackish-green-10"
+            onChange={handleSingleDateChange}
+          />
+        ) : (
+          <RangePicker
+            format={dateFormat}
+            onChange={handleDateRangeChange}
+            className="text-blackish-green-10"
+          />
+        )}
       </fieldset>
-      <fieldset
-        className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3 relative cursor-pointer"
-        onClick={handleClick}
-      >
-        <legend className="text-blackish-green text-sm capitalize">
-          passenger - class
-        </legend>
-        {showDropDown && <FlightDropdown />}
-      </fieldset>
+      <div className="relative">
+        <fieldset
+          className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3 cursor-pointer"
+          onClick={handleDropDownClick}
+        >
+          <legend className="text-blackish-green text-sm capitalize">
+            passenger - class
+          </legend>
+          <p className="w-full h-full capitalize text-blackish-green-10">
+            {totalPassengers > 0
+              ? `${totalPassengers} passenger${
+                  totalPassengers > 1 ? "s, " : ", "
+                }`
+              : ""}
+            {initialValues.cabinClass}
+          </p>
+        </fieldset>
+        <FlightDropdown
+          adultCount={initialValues.adultCount}
+          childrenCount={initialValues.childrenCount}
+          onAdultIncrement={handleAdultIncrement}
+          onAdultDecrement={handleAdultDecrement}
+          onChildrenIncrement={handleChildrenIncrement}
+          onChildrenDecrement={handleChildrenDecrement}
+          cabinType={initialValues.cabinClass}
+          onCabinClassChange={handleCabinClassChange}
+          showDropDown={showDropDown}
+          onClose={handleDropDownClick}
+        />
+      </div>
     </div>
   );
 };
