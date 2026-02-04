bb35a9aa0b6f5c75e3ddd1bc95a42d370fafcc1f
Anita Ikediashi <anitaikediashi83@gmail.com>
2026-02-04 04:08:33 +0100

    modified responsiveness for small screens

---
+++ components/icons/view_more.tsx
@@ -0,0 +1,27 @@
+
+
+export const ViewMoreIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#112211"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="m 2 6.007812 c -1.105469 0 -2 0.894532 -2 2 c 0 1.101563 0.894531 2 2 2 s 2 -0.898437 2 -2 c 0 -1.105468 -0.894531 -2 -2 -2 z m 6 0 c -1.105469 0 -2 0.894532 -2 2 c 0 1.101563 0.894531 2 2 2 s 2 -0.898437 2 -2 c 0 -1.105468 -0.894531 -2 -2 -2 z m 6 0 c -1.105469 0 -2 0.894532 -2 2 c 0 1.101563 0.894531 2 2 2 s 2 -0.898437 2 -2 c 0 -1.105468 -0.894531 -2 -2 -2 z m 0 0"
+          fill="#112211"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+}
--- components/sections/flights/segment_data.tsx
+++ components/sections/flights/segment_data.tsx
@@ -1,3 +1,4 @@
+import { ViewMoreIcon } from "@/components/icons/view_more";
 import { Button } from "@/components/reusable/button";
 import { formatDateTime, getDuration } from "@/helpers/convertNumberToTime";
 import { FlightOffer } from "@/types/flight_type";
@@ -10,7 +11,7 @@ type segmentDataProps = {
 export const SegmentData = ({ offers }: segmentDataProps) => {
   if (!offers) return [];
   return (
-    <div className="w-full flex md:flex-row flex-col md:justify-between font-montserrat">
+    <div className="w-full flex flex-row justify-between items-center md:items-start gap-4 md:gap-0 font-montserrat">
       <div className="md:w-[calc(100%-25%)] w-full md:py-6 md:pr-4 md:border-r border-r-blackish-green/25 flex flex-col gap-y-4 justify-center">
         {offers.segments?.map((segment, index) => {
           if (!segment.legs) return null;
@@ -22,8 +23,11 @@ export const SegmentData = ({ offers }: segmentDataProps) => {
           return segment.legs?.map((leg) =>
             leg.carriers?.map((carrier) => {
               return (
-                <div key={index} className="flex justify-between">
-                  <div className="flex gap-6">
+                <div
+                  key={index}
+                  className="flex justify-between items-center md:items-start relative"
+                >
+                  <div className="md:flex md:gap-6 gap-1.5 hidden">
                     <Image
                       src={
                         carrier.logo?.trim() || "/flights/airplane-ticket.png"
@@ -42,10 +46,10 @@ export const SegmentData = ({ offers }: segmentDataProps) => {
                       <p className="text-sm opacity-40">{carrier.name}</p>
                     </div>
                   </div>
-                  <p className="text-sm font-semibold opacity-78">
+                  <p className="text-sm font-semibold opacity-78 hidden md:block">
                     {stopLabel}
                   </p>
-                  <div className="flex flex-col">
+                  <div className="md:flex flex-col hidden">
                     <p className="opacity-78 font-semibold">
                       {getDuration(
                         segment.departure_time,
@@ -57,25 +61,68 @@ export const SegmentData = ({ offers }: segmentDataProps) => {
                       {segment.arrival_airport_code}
                     </p>
                   </div>
+                  <div className="flex md:hidden flex-col items-center gap-0.5">
+                    <Image
+                      src={
+                        carrier.logo?.trim() || "/flights/airplane-ticket.png"
+                      }
+                      alt={`${carrier.name} logo`}
+                      width={24}
+                      height={20}
+                      loading="eager"
+                      className="w-6 h-5"
+                    />
+                    <small className="text-[10px] opacity-40">{carrier.name}</small>
+                  </div>
+                  <div className="flex md:hidden flex-col">
+                    <small className="text-xs font-semibold">
+                      {formatDateTime(segment.departure_time).slice(0, -1)}
+                    </small>
+                    <small className="text-xs opacity-40">
+                      {segment.departure_airport_code}
+                    </small>
+                  </div>
+                  <div className="md:hidden flex flex-col items-center gap-y-2">
+                    <hr className="min-w-16 bg-blackish-green opacity-50" />
+                    <small className="text-[10px] opacity-78">
+                      {getDuration(
+                        segment.departure_time,
+                        segment.arrival_time,
+                      )}
+                    </small>
+                  </div>
+                  <div className="flex md:hidden flex-col">
+                    <small className="text-xs font-semibold">
+                      {formatDateTime(segment.arrival_time).slice(0, -1)}
+                    </small>
+                    <small className="text-xs opacity-40">
+                      {segment.arrival_airport_code}
+                    </small>
+                  </div>
                 </div>
               );
             }),
           );
         })}
       </div>
-      <div className="md:w-1/4 w-full md:py-6 md:pl-4 flex flex-col justify-end">
-        <div className="mb-6 flex flex-col">
+      <div className="md:w-1/4 max-w-1/4 md:py-6 md:pl-4 flex flex-col justify-end">
+        <div className="md:mb-6 mb-4 flex flex-col">
           <small className="text-xs font-medium- opacity-75">
             Starting from
           </small>
-          <p className="text-salmon-100 font-bold text-2xl ">
+          <p className="text-salmon-100 font-bold md:text-2xl text-xl">
             ${offers.price_breakdown?.total?.amount ?? 0}
           </p>
         </div>
         <Button
           type="button"
-          className="capitalize h-12 w-full bg-mint-green-100 text-sm font-semibold flex items-center justify-center rounded hover:bg-blackish-green hover:text-white"
+          className="capitalize h-12 w-full bg-mint-green-100 text-sm font-semibold hidden md:flex items-center justify-center rounded hover:bg-blackish-green hover:text-white"
           label="view deal"
+        />
+        <Button
+          type="button"
+          className="capitalize h-10 w-full bg-mint-green-100 text-sm font-semibold flex md:hidden items-center justify-center rounded hover:bg-blackish-green hover:text-white"
+          icon={<ViewMoreIcon />}
         />
       </div>
     </div>
