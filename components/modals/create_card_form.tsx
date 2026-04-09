0fcf872012ee49f1687423d1a6f62945097c3ada
Anita Ikediashi <anitaikediashi83@gmail.com>
2026-04-09 03:32:07 +0100

    added toastify

--- components/modals/create_card_form.tsx
+++ components/modals/create_card_form.tsx
@@ -9,6 +9,7 @@ import { validateLuhn } from "@/utils/luhnCheck";
 import { inputClassName } from "@/utils/inputClassName";
 import { currentYearCentury } from "@/helpers/currentYearCentury";
 import { processCardAddition } from "@/lib/actions/card-actions";
+import { ToastContainer, toast } from "react-toastify";
 
 /**
  * Partial<T> is a built-in utility type that constructs a new type where all properties of the original type T are set to optional
@@ -144,7 +145,6 @@ export const CreateCardForm = ({
       newErrors.expDate = "Card has expired";
     }
 
-    
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors);
       setIsLoading(false);
@@ -162,23 +162,28 @@ export const CreateCardForm = ({
           country: "",
           saveCard: false,
         });
+        toast.success("Card added successfully!", {
+          position: "top-center",
+          closeOnClick: true,
+          theme: "dark",
+        });
       } else {
         const serverErrors: Partial<CardFormData> = {};
         const e = response.errors;
-        if (e?.cardNumber)
-          serverErrors.cardNumber = e.cardNumber._errors[0];
-        if (e?.cvc)
-          serverErrors.cvc = e.cvc._errors[0];
-        if (e?.expDate)
-          serverErrors.expDate = e.expDate._errors[0];
-        if (e?.cardName)
-          serverErrors.cardName = e.cardName._errors[0];
-        if (e?.country)
-          serverErrors.country = e.country._errors[0];
+        if (e?.cardNumber) serverErrors.cardNumber = e.cardNumber._errors[0];
+        if (e?.cvc) serverErrors.cvc = e.cvc._errors[0];
+        if (e?.expDate) serverErrors.expDate = e.expDate._errors[0];
+        if (e?.cardName) serverErrors.cardName = e.cardName._errors[0];
+        if (e?.country) serverErrors.country = e.country._errors[0];
         setErrors(serverErrors);
       }
     } catch (error) {
       console.error("Submission failed", error);
+      toast.error("Failed to add card.", {
+        position: "top-center",
+        closeOnClick: true,
+        theme: "dark",
+      });
       setErrors({ cardName: "Something went wrong. Please try again." });
     } finally {
       setIsLoading(false);
@@ -186,169 +191,172 @@ export const CreateCardForm = ({
   };
 
   return (
-    <div
-      className={`fixed inset-0 bg-black/30 backdrop-blur-xs flex items-center justify-center z-100 font-montserrat transition-opacity duration-300 ease-in-out ${
-        showCardForm ? "visible opacity-100" : "invisible opacity-0"
-      }`}
-    >
+    <>
       <div
-        className={`bg-white rounded-2xl shadow-light p-8 lg:px-16 lg:pb-16 lg:pt-[88.24px] w-full max-w-[640px] transition-all duration-500 ease-[cubic-bezier(0.23,1,0.32,1)] max-h-[90vh] flex flex-col ${
-          showCardForm
-            ? "opacity-100 translate-y-0"
-            : "opacity-0 translate-y-20"
+        className={`fixed inset-0 bg-black/30 backdrop-blur-xs flex items-center justify-center z-100 font-montserrat transition-opacity duration-300 ease-in-out ${
+          showCardForm ? "visible opacity-100" : "invisible opacity-0"
         }`}
       >
-        <div className="flex justify-end">
-          <div
-            className="cursor-pointer w-6 h-6 flex items-center justify-center rounded-full hover:bg-blackish-green-10/50 transition-colors duration-200"
-            onClick={onClose}
-          >
-            <CloseIcon />
+        <div
+          className={`bg-white rounded-2xl shadow-light p-8 lg:px-16 lg:pb-16 lg:pt-[88.24px] w-full max-w-[640px] transition-all duration-500 ease-[cubic-bezier(0.23,1,0.32,1)] max-h-[90vh] flex flex-col ${
+            showCardForm
+              ? "opacity-100 translate-y-0"
+              : "opacity-0 translate-y-20"
+          }`}
+        >
+          <div className="flex justify-end">
+            <div
+              className="cursor-pointer w-6 h-6 flex items-center justify-center rounded-full hover:bg-blackish-green-10/50 transition-colors duration-200"
+              onClick={onClose}
+            >
+              <CloseIcon />
+            </div>
           </div>
-        </div>
-        <div className="overflow-y-auto flex-1 min-h-0 hide_scroll">
-          <h2 className="font-bold text-2xl md:text-[4xl] text-black mb-12">
-            Add a new Card
-          </h2>
-          <form action="" onSubmit={handleCardSubmit}>
-            <div className="flex flex-col gap-y-6 mb-10">
-              <div className="relative">
-                <fieldset className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3 relative">
-                  <legend className="text-blackish-green text-sm capitalize">
-                    card number
-                  </legend>
-                  <input
-                    type="text"
-                    name="cardNumber"
-                    placeholder="1234 5678 9012 3456"
-                    className={inputClassName}
-                    value={cardFormData.cardNumber}
-                    onChange={handleCardInputChange}
-                  />
-                  {detectCardType !== "" && (
-                    <Image
-                      src={detectCardType.src}
-                      alt={detectCardType.alt}
-                      width={24}
-                      height={16}
-                      className="absolute z-10 top-0.5 right-4 w-6 h-4"
-                    />
-                  )}
-                </fieldset>
-                {errors.cardNumber && (
-                  <span className="text-red-500 text-xs absolute -bottom-4">
-                    {errors.cardNumber}
-                  </span>
-                )}
-              </div>
-              <div className="flex flex-col lg:flex-row gap-6">
-                <div className="relative lg:w-1/2 w-full">
-                  <fieldset className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3">
+          <div className="overflow-y-auto flex-1 min-h-0 hide_scroll">
+            <h2 className="font-bold text-2xl md:text-[4xl] text-black mb-12">
+              Add a new Card
+            </h2>
+            <form action="" onSubmit={handleCardSubmit}>
+              <div className="flex flex-col gap-y-6 mb-10">
+                <div className="relative">
+                  <fieldset className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3 relative">
                     <legend className="text-blackish-green text-sm capitalize">
-                      exp. date
+                      card number
                     </legend>
                     <input
                       type="text"
-                      placeholder="MM/YY"
-                      name="expDate"
+                      name="cardNumber"
+                      placeholder="1234 5678 9012 3456"
                       className={inputClassName}
-                      value={cardFormData.expDate}
+                      value={cardFormData.cardNumber}
                       onChange={handleCardInputChange}
                     />
+                    {detectCardType !== "" && (
+                      <Image
+                        src={detectCardType.src}
+                        alt={detectCardType.alt}
+                        width={24}
+                        height={16}
+                        className="absolute z-10 top-0.5 right-4 w-6 h-4"
+                      />
+                    )}
                   </fieldset>
-                  {errors.expDate && (
+                  {errors.cardNumber && (
                     <span className="text-red-500 text-xs absolute -bottom-4">
-                      {errors.expDate}
+                      {errors.cardNumber}
                     </span>
                   )}
                 </div>
-                <div className="relative lg:w-1/2 w-full">
+                <div className="flex flex-col lg:flex-row gap-6">
+                  <div className="relative lg:w-1/2 w-full">
+                    <fieldset className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3">
+                      <legend className="text-blackish-green text-sm capitalize">
+                        exp. date
+                      </legend>
+                      <input
+                        type="text"
+                        placeholder="MM/YY"
+                        name="expDate"
+                        className={inputClassName}
+                        value={cardFormData.expDate}
+                        onChange={handleCardInputChange}
+                      />
+                    </fieldset>
+                    {errors.expDate && (
+                      <span className="text-red-500 text-xs absolute -bottom-4">
+                        {errors.expDate}
+                      </span>
+                    )}
+                  </div>
+                  <div className="relative lg:w-1/2 w-full">
+                    <fieldset className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3">
+                      <legend className="text-blackish-green text-sm uppercase">
+                        cvc
+                      </legend>
+                      <input
+                        type="number"
+                        placeholder="123"
+                        name="cvc"
+                        className={inputClassName}
+                        value={cardFormData.cvc}
+                        onChange={handleCardInputChange}
+                      />
+                    </fieldset>
+                    {errors.cvc && (
+                      <span className="text-red-500 text-xs absolute -bottom-4">
+                        {errors.cvc}
+                      </span>
+                    )}
+                  </div>
+                </div>
+                <div className="relative">
                   <fieldset className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3">
-                    <legend className="text-blackish-green text-sm uppercase">
-                      cvc
+                    <legend className="text-blackish-green text-sm">
+                      Name on Card
                     </legend>
                     <input
-                      type="number"
-                      placeholder="123"
-                      name="cvc"
+                      type="text"
+                      placeholder="John Doe"
+                      name="cardName"
                       className={inputClassName}
-                      value={cardFormData.cvc}
+                      value={cardFormData.cardName}
                       onChange={handleCardInputChange}
                     />
                   </fieldset>
-                  {errors.cvc && (
+                  {errors.cardName && (
                     <span className="text-red-500 text-xs absolute -bottom-4">
-                      {errors.cvc}
+                      {errors.cardName}
                     </span>
                   )}
                 </div>
+                <div className="relative">
+                  <fieldset className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3 select_wrapper">
+                    <legend className="text-blackish-green text-sm">
+                      Country or Region
+                    </legend>
+                    <Select
+                      options={CountryOptions}
+                      value={cardFormData.country}
+                      allowClear
+                      showSearch
+                      className="w-full text-blackish-green-10"
+                      onChange={countrySearch}
+                    />
+                  </fieldset>
+                  {errors.country && (
+                    <span className="text-red-500 text-xs absolute -bottom-4">
+                      {errors.country}
+                    </span>
+                  )}
+                </div>
+                <div className="relative">
+                  <Checkbox
+                    name="saveCard"
+                    checked={cardFormData.saveCard}
+                    onChange={handleCheckedInfo}
+                    className="text-blackish-green-10"
+                  >
+                    Securely save my information for 1-click checkout
+                  </Checkbox>
+                </div>
               </div>
-              <div className="relative">
-                <fieldset className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3">
-                  <legend className="text-blackish-green text-sm">
-                    Name on Card
-                  </legend>
-                  <input
-                    type="text"
-                    placeholder="John Doe"
-                    name="cardName"
-                    className={inputClassName}
-                    value={cardFormData.cardName}
-                    onChange={handleCardInputChange}
-                  />
-                </fieldset>
-                {errors.cardName && (
-                  <span className="text-red-500 text-xs absolute -bottom-4">
-                    {errors.cardName}
-                  </span>
-                )}
+              <div className="w-full">
+                <Button
+                  type="submit"
+                  label={isLoading ? "processing..." : "add card"}
+                  className="bg-mint-green-100 capitalize text-sm font-semibold w-full mb-4 h-12 rounded"
+                />
+                <p className="text-center text-xs opacity-75">
+                  By confirming your subscription, you allow Stripe to charge
+                  your card for this payment and future payments in accordance
+                  with their terms. You can always cancel your subscription.
+                </p>
               </div>
-              <div className="relative">
-                <fieldset className="h-14 border border-blackish-green-20 rounded-tl-sm rounded-tr-sm pl-3 select_wrapper">
-                  <legend className="text-blackish-green text-sm">
-                    Country or Region
-                  </legend>
-                  <Select
-                    options={CountryOptions}
-                    value={cardFormData.country}
-                    allowClear
-                    showSearch
-                    className="w-full text-blackish-green-10"
-                    onChange={countrySearch}
-                  />
-                </fieldset>
-                {errors.country && (
-                  <span className="text-red-500 text-xs absolute -bottom-4">
-                    {errors.country}
-                  </span>
-                )}
-              </div>
-              <div className="relative">
-                <Checkbox
-                  name="saveCard"
-                  checked={cardFormData.saveCard}
-                  onChange={handleCheckedInfo}
-                  className="text-blackish-green-10"
-                >
-                  Securely save my information for 1-click checkout
-                </Checkbox>
-              </div>
-            </div>
-            <div className="w-full">
-              <Button
-                type="submit"
-                label={isLoading ? "processing..." : "add card"}
-                className="bg-mint-green-100 capitalize text-sm font-semibold w-full mb-4 h-12 rounded"
-              />
-              <p className="text-center text-xs opacity-75">
-                By confirming your subscription, you allow Stripe to charge your
-                card for this payment and future payments in accordance with
-                their terms. You can always cancel your subscription.
-              </p>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      <ToastContainer position="top-center" theme="dark" closeOnClick={true} />
+    </>
   );
 };
