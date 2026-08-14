fc0a8a7dfe9066ca097531e08953230de3aafd1e
Anita Ikediashi <anitaikediashi83@gmail.com>
2026-08-14 04:01:02 +0100

    installed jest

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "jest";
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  // Path to your Next.js app, loads next.config.js and .env files
+  dir: "./",
+});
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    // match your tsconfig paths, e.g. "@/*": ["./*"]
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+};
+
+// createJestConfig is exported this way to ensure next/jest can load the Next.js config, which is async
+export default createJestConfig(config);
